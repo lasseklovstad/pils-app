4a2fb31e74f91a661d21d2479771858d326cebe4
Lasse Narula <lasse.klovstad@gmail.com>
2024-10-05 12:21:51 +0200

    Add components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,17 @@
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
 export default function Home() {
-  return <div className="bg-slate-400 text-lg">Hei pils ny!</div>;
+  return (
+    <main className="p-2">
+      <div className="flex gap-2 items-end">
+        <div>
+          <Label>Navn på brygg</Label>
+          <Input placeholder="Eks: Winter IPA" />
+        </div>
+        <Button>Start nytt brygg</Button>
+      </div>
+    </main>
+  );
 }
