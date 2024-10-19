991df3572a7adc319d5c98a2ac7f272af30fd96e
Lasse Narula <43168787+lasseklovstad@users.noreply.github.com>
2024-10-19 19:41:19 +0200

    fix: Import failed (#34)

--- app/routes.ts
+++ app/routes.ts
@@ -5,7 +5,7 @@ import type { RouteConfig } from "@react-router/dev/routes";
 export const routes: RouteConfig = [
   index("routes/batch/HomePage.tsx"),
   route("batch/:batchId", "routes/batch/BatchDetailsPage.tsx"),
-  route("health", "routes/Health.ts"),
+  route("health", "routes/HealthPage.ts"),
   ...prefix("controller", [
     index("routes/controller/IndexPage.tsx"),
     route(":controllerId", "routes/controller/ControllerDetailsPage.tsx"),
