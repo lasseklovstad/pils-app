b0868ec53ab5a224bbbd4d524db8d9ccf2481dbf
laklov <lasse.klovstad@gmail.com>
2025-05-11 13:39:59 +0200

    Add form fields

--- app/routes/controller/shared/ControllerMenu.tsx
+++ app/routes/controller/shared/ControllerMenu.tsx
@@ -27,13 +27,19 @@ import { useIsPending } from "~/lib/useIsPending";
 
 import {
   deleteControllerIntent,
-  editNameIntent,
+  editControllerIntent,
   EditNameSchema,
   type action,
 } from "../ControllerDetailsPage";
 
 type Props = {
-  controller: { name: string; id: number };
+  controller: {
+    name: string;
+    id: number;
+    minDelayInSeconds: number;
+    avgTemperatureBufferSize: number;
+    hysteresis: number;
+  };
 };
 
 export const ControllerMenu = ({ controller }: Props) => {
@@ -43,7 +49,12 @@ export const ControllerMenu = ({ controller }: Props) => {
   const [form, fields] = useForm({
     // Sync the result of last submission only when the state is idle
     lastResult: navigation.state === "idle" ? lastResult?.result : null,
-    defaultValue: { name: controller.name },
+    defaultValue: {
+      name: controller.name,
+      avgTemperatureBufferSize: controller.avgTemperatureBufferSize,
+      hysteresis: controller.hysteresis,
+      minDelayInSeconds: controller.minDelayInSeconds,
+    },
     onValidate({ formData }) {
       return parseWithZod(formData, { schema: EditNameSchema });
     },
@@ -70,14 +81,14 @@ export const ControllerMenu = ({ controller }: Props) => {
               onSelect={(e) => e.preventDefault()}
               className="flex justify-between"
             >
-              Endre navn
+              Endre kontroller
               <Edit className="ml-2 size-4" />
             </DropdownMenuItem>
           </DialogTrigger>
           <DialogContent>
             <Form method="PUT" {...getFormProps(form)}>
               <DialogHeader>
-                <DialogTitle>Endre navn</DialogTitle>
+                <DialogTitle>Endre kontroller</DialogTitle>
               </DialogHeader>
               <Field
                 labelProps={{ children: "Navn på kontroller" }}
@@ -86,9 +97,46 @@ export const ControllerMenu = ({ controller }: Props) => {
                   autoComplete: "off",
                   ...getInputProps(fields.name, { type: "text" }),
                 }}
+                errors={fields.name.errors}
+              />
+              <Field
+                labelProps={{ children: "Forsinkelse i sekunder" }}
+                inputProps={{
+                  autoComplete: "off",
+                  ...getInputProps(fields.minDelayInSeconds, {
+                    type: "number",
+                  }),
+                }}
+                errors={fields.minDelayInSeconds.errors}
+              />
+              <Field
+                labelProps={{ children: "Hysterese temp" }}
+                inputProps={{
+                  autoComplete: "off",
+                  step: 0.1,
+                  ...getInputProps(fields.hysteresis, {
+                    type: "number",
+                  }),
+                }}
+                errors={fields.hysteresis.errors}
+              />
+              <Field
+                labelProps={{
+                  children: "Antall punkter til glidende gjennomsnitt",
+                }}
+                inputProps={{
+                  autoComplete: "off",
+                  ...getInputProps(fields.avgTemperatureBufferSize, {
+                    type: "number",
+                  }),
+                }}
               />
               <DialogFooter>
-                <Button type="submit" name="intent" value={editNameIntent}>
+                <Button
+                  type="submit"
+                  name="intent"
+                  value={editControllerIntent}
+                >
                   {isPending ? <Loader2 className="animate-spin" /> : <Save />}
                   Lagre
                 </Button>
